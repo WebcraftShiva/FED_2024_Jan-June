07812f3bef385c8b7e09f459da8299bd15376c14
webcraftShiva <shivam.webcraft@gmail.com>
2024-04-13 01:05:25 +0530

    Lecture 57

---
+++ Lecture 57/4C/hooks/src/components/Revision.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useRef, useState } from 'react'
+ 
+
+const Revision = () => {
+
+        const[ count,setCount]=useState(0);
+        const PrevCount=useRef('');
+        console.log(PrevCount)
+    
+        useEffect(()=>{
+            PrevCount.current=count;
+        })
+       
+        function handle()
+        {
+            setCount(PrevCount=>PrevCount+1);
+        }
+  return (
+      <div>
+        <h1> PrevCount : {PrevCount.current}</h1>
+        <h2> Count : {count}</h2>
+         <button onClick={handle}>increament</button> 
+    </div>
+  )
+}
+
+export default Revision
